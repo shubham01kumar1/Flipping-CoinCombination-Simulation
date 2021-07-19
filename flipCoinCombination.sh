5c57b6af53e815dd2a1809a30cde458e7ce5ac3b
shubham01kumar1 <shubham01kumar1@gmail.com>
2021-07-19 09:51:06 +0530

    calculating percentage of singlet combination feature

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -2,10 +2,20 @@
 
 HEAD=1
 TAIL=0
-simulate=$((RANDOM%2))
-if [ $simulate -eq $HEAD ]
-then
-	echo HEAD WINS
-else
-	echo TAIL WINS
+
+headWins=0
+tailWins=0
+for((i=0;i<20;i++))
+do
+	simulate=$((RANDOM%2))
+	if [ $simulate -eq $HEAD ]
+	then
+		((headWins++))
+	else
+		((tailWins++))
 fi
+done
+total_Simu=$((headWins + tailWins))
+percentage_OfHead=$(echo $headWins $total_Simu | awk '{print $1/$2 *100}')
+
+percentage_OfTail=$(echo $tailWins $total_Simu | awk '{print $1/$2 *100}')
