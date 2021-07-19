e944b09a8819b38ca5806f4356f8cd0bae703be8
shubham01kumar1 <shubham01kumar1@gmail.com>
2021-07-19 10:47:37 +0530

    storing result in dictionary and determining percentage of doublet combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,21 +1,35 @@
 #!/bin/bash -x
 declare -A dict
-HEAD=1
-TAIL=0
 
-dict[headWins]=0
-dict[tailWins]=0
-for((i=0;i<50;i++))
+TH=3
+HT=2
+HH=1
+TT=0
+
+dict[HH]=0
+dict[TT]=0
+dict[TH]=0
+dict[HT]=0
+
+for((i=0;i<100;i++))
 do
-	simulate=$((RANDOM%2))
-	if [ $simulate -eq $HEAD ]
-	then
-		dict[headWins]=$((${dict[headWins]}+1))
-	else
-		dict[tailWins]=$((${dict[tailWins]}+1))
-fi
+	simulate=$(( RANDOM % 4 ))
+	case $simulate in
+		$HH)
+			dict[HH]=$((${dict[HH]}+1));;
+		$HT)
+			dict[HT]=$((${dict[HT]}+1));;
+		$TH)
+			dict[TH]=$((${dict[TH]}+1));;
+		*)
+			dict[TT]=$((${dict[TT]}+1));;
+	esac
 done
-total_Simu=$(( dict["headWins"] + dict[tailWins] ))
-percentage_OfHead=$(echo ${dict["headWins"]} $total_Simu | awk '{print $1/$2 *100}')
+total_Simu=$(( dict[HH] + dict[HT] + dict[TH] + dict[TT]))
+percentage_OfHH=$(echo ${dict[HH]} $total_Simu | awk '{print $1/$2 *100}')
 
-percentage_OfTail=$(echo ${dict["tailWins"]} $total_Simu | awk '{print $1/$2 *100}')
+percentage_OfHT=$(echo ${dict[HT]} $total_Simu | awk '{print $1/$2 *100}')
+
+percentage_OfTH=$(echo ${dict[TH]} $total_Simu | awk '{print $1/$2 *100}')
+
+percentage_OfTT=$(echo ${dict[TT]} $total_Simu | awk '{print $1/$2 *100}')
