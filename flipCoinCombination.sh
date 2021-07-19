37a24beabf4420852263e5ac75aa15b0d5b37aa4
shubham01kumar1 <shubham01kumar1@gmail.com>
2021-07-19 09:41:24 +0530

    simulation a coin flip feature

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,3 +1,11 @@
 #!/bin/bash -x
 
-echo "Welcome to the Coin COmbination flipping problem"
+HEAD=1
+TAIL=0
+simulate=$((RANDOM%2))
+if [ $simulate -eq $HEAD ]
+then
+	echo HEAD WINS
+else
+	echo TAIL WINS
+fi
